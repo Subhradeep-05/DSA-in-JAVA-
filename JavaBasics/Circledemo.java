fa9a90631d7a3232c58d1a6d6f8eb3a667f0499a
Subhradeep-05 <174829395+Subhradeep-05@users.noreply.github.com>
2025-10-07 09:04:12 +0530

    Add my Java codes folder

---
+++ JavaBasics/Circledemo.java
@@ -0,0 +1,25 @@
+package JavaBasics;
+class Circle {
+    double x, y, r;
+    
+    double circumference(){
+        return 2*3.14*r;
+    };
+
+    double area(){
+        return 3.14*r*r;
+    };
+}
+
+class Circledemo {
+    public static void main(String args[]){
+        Circle c = new Circle();
+        c.x = 0.0;
+        c.y = 0.0;
+        c.r = 12.15;
+
+        System.out.println("Cicumference of the circle is:  "+ c.circumference());
+        System.out.println(("Area of the circle is: "+ c.area()));
+    }
+    
+}
---
+++ JavaBasics/JavaBasics.java
@@ -0,0 +1,93 @@
+
+// public class new{
+//     public static void main(String args[]){
+//         System.out.print("Hello World");
+//     }
+// }
+
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]) {
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+        // int a = 10;
+        // int b = 5;
+        // System.out.print(2*(a+b));
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();//next- before space
+        // System.out.print(input);
+
+        // String input = sc.nextLine();//next- after space
+        // System.out.print(input);
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product= a*b;
+        // System.out.print(product);
+
+
+        //area of a circle
+        // float rad = sc.nextFloat();
+        // double area = 3.14 * rad * rad;
+        // System.out.print(area);
+
+
+        // float a = 25.76f;
+        // int b = (int) a; //Type Casting
+        // System.out.print(b);
+
+        //Print Largest among 2 numbers
+        // int a = 1;
+        // int b = 5;
+        // if(a>b){
+        //     System.out.print("a i bigger and the number is:");
+    
+        // }
+        // else{
+        //     System.out.println("The bigger number is b and the number is;");
+        // }
+
+        //Odd or even
+        // System.out.print("Enter the number:");
+        // int num = sc.nextInt();
+        // if(num%2==0){
+        //     System.out.println("The number is even");
+        // }
+        // else{
+        //     System.out.println("The number is odd");
+        // }
+
+        //income tax calculator
+        // System.out.print("Enter your income in rs.");
+        // int income = sc.nextInt();
+        // if (income<500000){
+        //     System.out.println("You have 0% Tax to pay.");
+        // }
+        // else if(income>=500000 && income<1000000){
+        //     System.out.println("You have 20% Tax to pay.");
+        // }
+        // else if(income > 1000000){
+        //     System.out.println("You have 0% Tax to pay.");
+        // }
+
+
+        //pass or fail using ternary operator
+        // System.out.print("Enter the student marks: ");
+        // int marks= sc.nextInt();
+        // String result = (marks>40?"pass":"fail");
+        // System.out.println("The result of the studnet is-  "+result);
+
+
+        // System.out.print("Enter a number:");
+        // int n = sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     System.out.println(i);
+            
+        // }
+
+        
+        
+    }
+}
