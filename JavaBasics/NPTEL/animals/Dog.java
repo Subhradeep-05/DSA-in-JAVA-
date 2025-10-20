316390ae3a5e56a90044576effc8952d161e6f54
Subhradeep-05 <174829395+Subhradeep-05@users.noreply.github.com>
2025-10-20 20:36:17 +0530

    Interface

---
+++ JavaBasics/NPTEL/animals/Dog.java
@@ -0,0 +1,13 @@
+package animals;
+
+public class Dog implements Animal {
+    public void eat() {
+        System.out.println("Dog eats bones");
+    }
+
+    public void sleep() {
+        System.out.println("Dog sleeps in kennel");
+    }
+
+    
+}
---
+++ JavaBasics/NPTEL/animals/Main.java
@@ -0,0 +1,13 @@
+package animals;
+// File: Animal.java
+
+// File: Dog.java
+
+// File: Main.java
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.eat();
+        myDog.sleep();
+    }
+}
